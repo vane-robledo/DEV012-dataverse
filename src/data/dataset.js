001e770e28aa2f0084cfb9d225f5a2f2915f63a5
Diveana Inciarte <dinciarte25@gmail.com>
2023-10-02 11:56:07 -0300

    Se añaden las url de imágenes

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -4,7 +4,7 @@ export default [
       name: "Monkey D. Luffy",
       shortDescription: "Capitán de los Piratas del Sombrero de Paja",
       description: "Luffy es el intrépido capitán de los Piratas del Sombrero de Paja.",
-      imageUrl: "url_de_la_imagen_luffy.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/a/af/Monkey_D._Luffy_Anime_Dos_A%C3%B1os_Despu%C3%A9s_Infobox.png/revision/latest/scale-to-width-down/239?cb=20200616015904&path-prefix=es",
       facts: {
         bounty: "1,500,000,000 Berries",
         status: "Vivo",
@@ -17,7 +17,7 @@ export default [
       name: "Roronoa Zoro",
       shortDescription: "Espadachín de los Piratas del Sombrero de Paja",
       description: "Zoro es el espadachín principal de los Piratas del Sombrero de Paja.",
-      imageUrl: "url_de_la_imagen_zoro.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/5/56/Roronoa_Zoro_Anime_Dos_A%C3%B1os_Despu%C3%A9s_Infobox.png/revision/latest/scale-to-width-down/188?cb=20190826160007&path-prefix=es",
       facts: {
         bounty: "320,000,000 Berries",
         status: "Vivo",
@@ -30,7 +30,7 @@ export default [
       name: "Nami",
       shortDescription: "Navegante experta de los Piratas del Sombrero de Paja",
       description: "Nami es la talentosa navegante de los Piratas del Sombrero de Paja.",
-      imageUrl: "url_de_la_imagen_nami.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/0/0c/Nami_Anime_Dos_A%C3%B1os_Despu%C3%A9s_Infobox.png/revision/latest/scale-to-width-down/298?cb=20191125191158&path-prefix=es",
       facts: {
         bounty: "66,000,000 Berries",
         status: "Viva",
@@ -69,7 +69,7 @@ export default [
       name: "Tony Tony Chopper",
       shortDescription: "Médico y reno con una Fruta del Diablo",
       description: "Chopper es el adorable médico y reno con una Fruta del Diablo en los Piratas del Sombrero de Paja.",
-      imageUrl: "url_de_la_imagen_chopper.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/9/92/Usopp_Anime_Dos_A%C3%B1os_Despu%C3%A9s_Infobox.png/revision/latest/scale-to-width-down/260?cb=20160919181926&path-prefix=es",
       facts: {
         bounty: "100 Berries",
         status: "Vivo",
@@ -82,7 +82,7 @@ export default [
       name: "Nico Robin",
       shortDescription: "Arqueóloga y conocedora de la Fruta del Diablo",
       description: "Robin es la enigmática arqueóloga y usuaria de la Fruta del Diablo en los Piratas del Sombrero de Paja.",
-      imageUrl: "url_de_la_imagen_robin.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/9/9a/Nico_Robin_Anime_Dos_A%C3%B1os_Despu%C3%A9s_Infobox.png/revision/latest/scale-to-width-down/257?cb=20200902132447&path-prefix=es",
       facts: {
         bounty: "130,000,000 Berries",
         status: "Viva",
@@ -95,7 +95,7 @@ export default [
       name: "Franky",
       shortDescription: "Carpintero y cyborg",
       description: "Franky es el excéntrico carpintero y cyborg de los Piratas del Sombrero de Paja.",
-      imageUrl: "url_de_la_imagen_franky.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/a/a5/Franky_Anime_Dos_A%C3%B1os_Despu%C3%A9s_Infobox.png/revision/latest?cb=20121220031313&path-prefix=es",
       facts: {
         bounty: "94,000,000 Berries",
         status: "Vivo",
@@ -108,7 +108,7 @@ export default [
       name: "Brook",
       shortDescription: "Músico esqueleto",
       description: "Brook es el alegre músico esqueleto de los Piratas del Sombrero de Paja.",
-      imageUrl: "url_de_la_imagen_brook.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/0/06/Brook_Anime_Dos_A%C3%B1os_Despu%C3%A9s_Infobox.png/revision/latest/scale-to-width-down/214?cb=20200323143522&path-prefix=es",
       facts: {
         bounty: "83,000,000 Berries",
         status: "Vivo (no muerto)",
@@ -121,7 +121,7 @@ export default [
       name: "Jimbei",
       shortDescription: "Antiguo capitán pirata y experto en artes marciales",
       description: "Jimbei es el sabio antiguo capitán pirata y experto en artes marciales de los Piratas del Sombrero de Paja.",
-      imageUrl: "url_de_la_imagen_jimbei.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/8/81/Jinbe_Anime_Infobox.png/revision/latest?cb=20170521165842&path-prefix=es",
       facts: {
         bounty: "250,000,000 Berries (anteriormente)",
         status: "Vivo",
@@ -134,7 +134,7 @@ export default [
       name: "Vinsmoke Sanji",
       shortDescription: "Príncipe renegado y cocinero",
       description: "Sanji, también conocido como Vinsmoke Sanji, es el príncipe renegado y apasionado cocinero de los Piratas del Sombrero de Paja.",
-      imageUrl: "url_de_la_imagen_sanji.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/6/66/Sanji_Anime_Dos_A%C3%B1os_Despu%C3%A9s_Infobox.png/revision/latest/scale-to-width-down/167?cb=20191029181107&path-prefix=es",
       facts: {
         bounty: "330,000,000 Berries",
         status: "Vivo",
@@ -147,7 +147,7 @@ export default [
       name: "Carrot",
       shortDescription: "Guerrera mink y compañera temporal",
       description: "Carrot es la valiente guerrera mink y compañera temporal de los Piratas del Sombrero de Paja en su viaje a Whole Cake Island y Wano.",
-      imageUrl: "url_de_la_imagen_carrot.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/e/e2/Carrot_Anime_Infobox.png/revision/latest/scale-to-width-down/264?cb=20170219170403&path-prefix=es",
       facts: {
         bounty: "Desconocido",
         status: "Viva",
@@ -160,7 +160,7 @@ export default [
       name: "Dracule Mihawk",
       shortDescription: "Mejor espadachín del mundo",
       description: "Dracule Mihawk es el formidable mejor espadachín del mundo y poseedor de la espada Kokuto Yoru.",
-      imageUrl: "url_de_la_imagen_mihawk.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/b/bf/Dracule_Mihawk_Anime_Infobox.png/revision/latest?cb=20220612102336&path-prefix=es",
       facts: {
         bounty: "Desconocido",
         status: "Vivo",
@@ -173,7 +173,7 @@ export default [
       name: "Edward Newgate",
       shortDescription: "Poderoso Yonkou, 'Barba Blanca'",
       description: "Edward Newgate, también conocido como 'Barba Blanca', fue uno de los Yonkou más poderosos y valorados.",
-      imageUrl: "url_de_la_imagen_barbablanca.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/b/b7/Edward_Newgate_Anime_Infobox.png/revision/latest?cb=20130602201437&path-prefix=es",
       facts: {
         bounty: "Desconocido",
         status: "Fallecido",
@@ -186,7 +186,7 @@ export default [
       name: "Boa Hancock",
       shortDescription: "Emperatriz pirata y usuaria de la Fruta del Diablo 'Mero Mero no Mi'",
       description: "Boa Hancock es la temida Emperatriz Pirata y usuaria de la Fruta del Diablo 'Mero Mero no Mi'.",
-      imageUrl: "url_de_la_imagen_hancock.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/f/f0/Boa_Hancock_Anime_Infobox.png/revision/latest/scale-to-width-down/262?cb=20220707114317&path-prefix=es",
       facts: {
         bounty: "Desconocido",
         status: "Viva",
@@ -199,7 +199,7 @@ export default [
       name: "Portgas D. Ace",
       shortDescription: "Hermano de Luffy y comandante de los Piratas Spade",
       description: "Portgas D. Ace fue el carismático hermano de Luffy y comandante de los Piratas Spade.",
-      imageUrl: "url_de_la_imagen_ace.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/4/4f/Portgas_D._Ace_Anime_Infobox.png/revision/latest/scale-to-width-down/262?cb=20220330162003&path-prefix=es",
       facts: {
         bounty: "Desconocido",
         status: "Fallecido",
@@ -212,7 +212,7 @@ export default [
       name: "Trafalgar Law",
       shortDescription: "Cirujano de la muerte y aliado de los Piratas del Sombrero de Paja",
       description: "Trafalgar Law es el hábil cirujano de la muerte y aliado ocasional de los Piratas del Sombrero de Paja.",
-      imageUrl: "url_de_la_imagen_law.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/2/20/Trafalgar_D._Water_Law_Anime_Dos_A%C3%B1os_Despu%C3%A9s_Infobox.png/revision/latest/scale-to-width-down/224?cb=20131208122908&path-prefix=es",
       facts: {
         bounty: "500,000,000 Berries",
         status: "Vivo",
@@ -225,10 +225,10 @@ export default [
       name: "Charlotte Katakuri",
       shortDescription: "Comandante dulce y experto en la Fruta del Diablo 'Mochi Mochi no Mi'",
       description: "Charlotte Katakuri es el formidable comandante dulce y usuario de la Fruta del Diablo 'Mochi Mochi no Mi'.",
-      imageUrl: "url_de_la_imagen_katakuri.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/2/2e/Charlotte_Katakuri_Anime_Infobox.png/revision/latest?cb=20230404162130&path-prefix=es",
       facts: {
         bounty: "1,057,000,000 Berries",
-        status: "Fallecido",
+        status: "Vivo",
         seaOfOrigin: "New World",
         crewOrigin: "Big Mom Pirates"
       }
@@ -238,7 +238,7 @@ export default [
       name: "Marshall D. Teach",
       shortDescription: "Capitán de los Piratas Barbanegra",
       description: "Marshall D. Teach, también conocido como Barbanegra, es el maestro del caos y capitán de los Piratas Barbanegra.",
-      imageUrl: "url_de_la_imagen_blackbeard.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/6/6b/Marshall_D._Teach_Anime_Debut_Infobox.png/revision/latest?cb=20201020133237&path-prefix=es",
       facts: {
         bounty: "Desconocido",
         status: "Vivo",
@@ -251,7 +251,7 @@ export default [
       name: "Jewelry Bonney",
       shortDescription: "Pirata con la habilidad de la Fruta del Diablo 'Sorosoro no Mi'",
       description: "Jewelry Bonney es una pirata con la rara habilidad de la Fruta del Diablo 'Sorosoro no Mi'.",
-      imageUrl: "url_de_la_imagen_bonney.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/1/16/Jewelry_Bonney_Anime_Dos_A%C3%B1os_Despu%C3%A9s_Infobox.png/revision/latest?cb=20190816115635&path-prefix=es",
       facts: {
         bounty: "Desconocido",
         status: "Viva",
@@ -264,7 +264,7 @@ export default [
       name: "Buggy el Payaso",
       shortDescription: "Capitán de los Piratas Buggy",
       description: "Buggy el Payaso es el excéntrico capitán de los Piratas Buggy.",
-      imageUrl: "url_de_la_imagen_buggy.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/8/80/Buggy_Anime_Debut_Infobox.png/revision/latest?cb=20130811205903&path-prefix=es",
       facts: {
         bounty: "Desconocido",
         status: "Vivo",
@@ -277,7 +277,7 @@ export default [
       name: "Gol D. Roger",
       shortDescription: "El legendario Rey de los Piratas",
       description: "Gol D. Roger es el legendario Rey de los Piratas y el descubridor del One Piece.",
-      imageUrl: "url_de_la_imagen_roger.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/2/24/Gol_D._Roger_Anime_Infobox.png/revision/latest?cb=20120317163820&path-prefix=es",
       facts: {
         bounty: "Desconocido",
         status: "Fallecido",
@@ -290,7 +290,7 @@ export default [
       name: "Smoker",
       shortDescription: "Vicealmirante de la Marina",
       description: "Smoker es el implacable vicealmirante de la Marina conocido por perseguir a los piratas con su astuto sentido de la justicia.",
-      imageUrl: "url_de_la_imagen_smoker.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/6/65/Smoker_Anime_Debut_Infobox.png/revision/latest?cb=20210203143045&path-prefix=es",
       facts: {
         bounty: "Desconocido",
         status: "Vivo",
@@ -303,7 +303,7 @@ export default [
       name: "Sir Crocodile",
       shortDescription: "Ex-Shichibukai y líder de Baroque Works",
       description: "Sir Crocodile es el astuto ex-Shichibukai y líder de la organización criminal Baroque Works.",
-      imageUrl: "url_de_la_imagen_crocodile.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/f/fd/Crocodile_Anime_Infobox.png/revision/latest?cb=20161216184552&path-prefix=es",
       facts: {
         bounty: "81,000,000 Berries (anteriormente)",
         status: "Vivo",
@@ -316,7 +316,7 @@ export default [
       name: "Eustass Kid",
       shortDescription: "Capitán de los Piratas Kid",
       description: "Eustass Kid es el temible capitán de los Piratas Kid y usuario de la Fruta del Diablo 'Jiki Jiki no Mi'.",
-      imageUrl: "url_de_la_imagen_kid.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/9/97/Eustass_Kid_Anime_Debut_Infobox.png/revision/latest?cb=20100729224742&path-prefix=es",
       facts: {
         bounty: "Desconocido",
         status: "Vivo",
@@ -329,7 +329,7 @@ export default [
       name: "Shanks",
       shortDescription: "El poderoso y amistoso Yonkou",
       description: "Shanks es el carismático y poderoso Yonkou conocido por su amistad con Luffy.",
-      imageUrl: "url_de_la_imagen_shanks.jpg",
+      imageUrl: "https://static.wikia.nocookie.net/onepiece/images/6/66/Shanks_Anime_Infobox.png/revision/latest?cb=20191214223142&path-prefix=es",
       facts: {
         bounty: "Desconocido",
         status: "Vivo",
