13a1de761b75a2dc1ac8e5193977d3c678e583e5
Diveana Inciarte <dinciarte25@gmail.com>
2023-10-04 01:11:57 -0300

    Se añade función de render para la data y se añade corrección final de url de imágenes.

--- src/view.js
+++ src/view.js
@@ -1,4 +1,16 @@
 export const renderItems = (data) => {
+  data.forEach((element) => {
+    const li = document.createElement("li");
+    const arr = document.querySelector("ul");
+    li.innerHTML = `
+      <h2>${element.name}</h2>
+      <img src='${element.imageUrl}' alt="${element.id}">
+      <p>${element.description}</p>
+      <p>${element.facts.bounty}</p>
+      `;
+    arr.appendChild(li);
+    return arr;
+  });
 
+  //eslint-disable-next-line no-console
 };
-// eslint-disable-next-line no-console
