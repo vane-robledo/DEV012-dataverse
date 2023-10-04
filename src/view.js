6596fb0142f39c4a0a7218947383792312724834
Diveana Inciarte <dinciarte25@gmail.com>
2023-10-03 23:41:46 -0300

    Cambios finales en imágenes de dataset.js con formato correcto para tamaño de tarjetas.

--- src/view.js
+++ src/view.js
@@ -1,13 +1,4 @@
 export const renderItems = (data) => {
-  const li = document.createElement("li");
-  li.innerHTML = data;
-  return li;
 
 };
-const arr = document.querySelector("ul");
-
-arr.appendChild(renderItems("Prueba1"))
-
-
 // eslint-disable-next-line no-console
-
