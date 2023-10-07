7489be97ce049fb457702b626b2bfc92a6b1ba74
vanelopezr <vanne.lopez.r@gmail.com>
2023-10-06 22:54:38 -0300

    sort

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,16 @@
-// Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
-
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return [];
+export const sortData = (data, sortBy, sortOrder) => {
+  const sortAsc = data.sort((x, y) => {
+    if (x.name > y.name) {
+      if (sortOrder === "asc") {
+        return 1;
+      } else {
+        return -1;
+      }
+    } 
+    return 0; 
+  });
+  if (sortOrder === "desc") {
+    sortAsc.reverse();
+  }
+  return sortAsc;
 };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import { example } from './dataFunctions.js';
+import { sortData } from './dataFunctions.js';
 import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
-// eslint-disable-next-line no-console
-console.log(example, renderItems(data), data);
+const rootRender = document.querySelector("#root");
+rootRender.appendChild(renderItems(data));
+
+const sort = document.querySelector('[data-testid="select-sort"]');
+
+sort.addEventListener("change", () => {
+  const sortOrder = sort.value;
+  const sortedData = sortData(data,"name", sortOrder)
+
+  rootRender.innerHTML = "";
+  const sortedList = renderItems(sortedData);
+  rootRender.appendChild(sortedList)
+});
 
