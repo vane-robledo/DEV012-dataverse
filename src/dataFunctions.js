29207248642234ffe77a94b372b6e2a514ff9aae
Diveana Inciarte <dinciarte25@gmail.com>
2023-10-10 20:57:50 -0300

    Pruebas de función sortBounty

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -6,11 +6,33 @@ export const sortData = (data, sortBy, sortOrder) => {
       } else {
         return -1;
       }
-    } 
-    return 0; 
+    }
+    return 0;
   });
   if (sortOrder === "desc") {
     sortAsc.reverse();
   }
   return sortAsc;
+};
+
+export const sortBounty = (data, sortBy, sortOrder) => {
+  const dataFacts = data
+    .flatMap((element) => element.facts)
+    .map((element) => element.bounty);
+
+  const dataNumber = dataFacts.map((bounty) => {
+    const bountyWithDot = bounty.replace(/,/g, "");
+    return parseFloat(bountyWithDot);
+  });
+
+  const sortBountyOrder = dataNumber.sort((a, b) => {
+    if (sortOrder === "bounty-asc") {
+      return a - b;
+    } else if (sortOrder === "bounty-desc") {
+      return b - a;
+    }
+    return 0;
+  });
+  console.log(sortBountyOrder);
+  return sortBountyOrder;
 };
