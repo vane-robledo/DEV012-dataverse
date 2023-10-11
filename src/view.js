2265d3430921604754222efdd1def2eacfedd05c
Diveana Inciarte <dinciarte25@gmail.com>
2023-10-10 23:30:15 -0300

    Cambios en CSS

--- src/view.js
+++ src/view.js
@@ -5,16 +5,16 @@ export const renderItems = (data) => {
   data.forEach((element) => {
     const li = document.createElement("li");
     li.classList.add("cards")
-    const arr = document.querySelector("#root")
-  
+    
     li.innerHTML = `
-      <h3 itemprop="name">${element.name}</h3>
-      <img src="${element.imageUrl}" alt="${element.id}">
-      <p itemprop="description">${element.description}</p>
-      <h4 itemprop="bounty">${element.facts.bounty}</h4>
-      `;
+    <h3 itemprop="name">${element.name}</h3>
+    <img src="${element.imageUrl}" alt="${element.id}">
+    <p itemprop="description">${element.description}</p>
+    <h4 itemprop="bounty">${element.facts.bounty}</h4>
+    `;
+    const root = document.querySelector("#root")
     ul.appendChild(li);
-    arr.appendChild(ul)
+    root.appendChild(ul)
   });
   return ul;
 };
