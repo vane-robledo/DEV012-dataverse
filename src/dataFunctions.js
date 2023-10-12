ec0131a0c5fe24c81e42c71e9a832d8b5a3d1833
Diveana Inciarte <dinciarte25@gmail.com>
2023-10-11 23:25:33 -0300

    Se arregla funció nde sort. Se añade función al botón de clear selection que elimina lo seleccionado en los filtros y el sort. También, pequeños arreglos al CSS mobile.

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,28 +1,19 @@
+
 export const sortData = (data, sortBy, sortOrder) => {
-  const sortAsc = data.sort((x, y) => {
-    if (x[sortBy] > y[sortBy]) {
-      if (sortOrder === "asc") {
-        return 1;
-      } else {
-        return -1;
-      }
-    }
-    return 0;
-  });
-  if (sortOrder === "desc") {
-    sortAsc.reverse();
+  const sortedAsc = data.toSorted((a, b) => a[sortBy] > b[sortBy]);
+  const sortedDesc = data.toSorted((a, b) => a[sortBy] < b[sortBy]);
+  if (sortOrder === "asc") {
+    return sortedAsc;
+  } else{
+    return sortedDesc;
   }
-  return sortAsc;
 };
 
 export const filterData = (data, filterBy, value) => {
-  return data.filter(item => {
+  return data.filter((item) => {
     if (item.facts[filterBy]) {
       return item.facts[filterBy] === value;
     }
     return false;
   });
-}
-
-
-
+};
