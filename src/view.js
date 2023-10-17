f0215dc280183bb6218ef6de7b7a1c81c097b0bd
vanelopezr <vanne.lopez.r@gmail.com>
2023-10-17 14:21:06 -0300

    view renderStats

--- src/view.js
+++ src/view.js
@@ -1,6 +1,5 @@
 export const renderItems = (data) => {
   const ul = document.createElement("ul");
-  
   data.forEach((element) => {
     const li = document.createElement("li");
     li.setAttribute("itemscope", "");
@@ -19,32 +18,47 @@ export const renderItems = (data) => {
   return ul;
 };
 export const renderStats = () => {
-  const ul = document.createElement("ul");
-  const liOrigin = document.createElement("li");
-  const liCrew = document.createElement("li");
-  const liBounty = document.createElement("li");
-  liCrew.classList.add("cards");
-  liOrigin.classList.add("cards");
-  liBounty.classList.add("cards");
-  liOrigin.innerHTML = `
-  <h3>Origin</h3>
-  <img src="https://cdn.myanimelist.net/s/common/uploaded_files/1447350221-41774e2d831c741252034f3e287dc61d.jpeg" alt="">
-  <button class="more-info">More Info</button>
-  `;
-  liCrew.innerHTML = `
-  <h3>Crew</h3>
-  <img src="https://i.pinimg.com/originals/ff/e8/e8/ffe8e84d96f9417fec86d2b84470a0b6.jpg" alt="">
-  <button class="more-info">More Info</button>
-  `;
-  liBounty.innerHTML = `
-  <h3>Bounty</h3>
-  <img src="https://birdsofherme.files.wordpress.com/2021/08/sanji-bounty.jpg" alt="">
-  <button class="more-info">More Info</button>
-  `;
   const root = document.querySelector("#root");
+  const ul = document.createElement("ul");
+  //Render Origin Card
+  const liOrigin = document.createElement("li");
+  liOrigin.classList.add("cards");
+  const h3Origin = document.createElement("h3");
+  h3Origin.innerHTML = "Origin Fact";
+  const imgOrigin = document.createElement("img");
+  imgOrigin.classList.add("img-origin");
+  const pOrigin = document.createElement("p");
+  pOrigin.id = "origin";
+  liOrigin.appendChild(h3Origin);
+  liOrigin.appendChild(imgOrigin);
+  liOrigin.appendChild(pOrigin);
   ul.appendChild(liOrigin);
+  //Render Crew Card
+  const liCrew = document.createElement("li");
+  liCrew.classList.add("cards");
+  const h3Crew = document.createElement("h3");
+  h3Crew.innerHTML = "Crew Fact";
+  const imgCrew = document.createElement("img");
+  imgCrew.classList.add("img-crew");
+  const pCrew = document.createElement("p");
+  pCrew.id = "crew";
+  liCrew.appendChild(h3Crew);
+  liCrew.appendChild(imgCrew);
+  liCrew.appendChild(pCrew);
   ul.appendChild(liCrew);
+  //Render Bounty Card
+  const liBounty = document.createElement("li");
+  liBounty.classList.add("cards");
+  const h3Bounty = document.createElement("h3");
+  h3Bounty.innerHTML = "Bounty Fact";
+  const imgBounty = document.createElement("img");
+  imgBounty.classList.add("img-Bounty");
+  const pBounty = document.createElement("p");
+  pBounty.id = "bounty";
+  liBounty.appendChild(h3Bounty);
+  liBounty.appendChild(imgBounty);
+  liBounty.appendChild(pBounty);
   ul.appendChild(liBounty);
   root.appendChild(ul);
   return ul;
-}
+};
