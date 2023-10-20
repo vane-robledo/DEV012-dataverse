5d9cf4843ced9a3ea6d25863a47ad41eec058ffd
Diveana Inciarte <dinciarte25@gmail.com>
2023-10-20 11:33:55 -0300

    Pequeño cambio en archivo dataFunctions.js

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -48,30 +48,17 @@ export const computeStats = (data, property, value) => {
   const percentage = (reduceData / data.length) * 100;
   return percentage.toFixed(2);
 };
-// export const computeStatsBounty = (data, property, value) => {
-//   const mapped = data.map(element => element.facts[property] >= value ? 1 : 0);
-//   const reduceData = mapped.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-//   const percentage = (reduceData / data.length) * 100;
-//   return percentage.toFixed(2);
-// };
 
 export const computeStatsBounty = (data, bounty, value) => {
   const bountyParsed = data.map(element => {
-    // Ensure the data exists before attempting to access the nested objects.
     if (element.facts && element.facts[bounty]) {
       return parseInt(element.facts[bounty].replace(/[^0-9]/g, ''), 10);
     } else {
-      return 0; // Handle cases where the data or the specified property doesn't exist.
+      return 0;
     }
   });
-
-  console.log(bountyParsed);
-
   const mapped = bountyParsed.map(parsedValue => (parsedValue >= value ? 1 : 0));
-  console.log(mapped);
-
   const reduceData = mapped.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-
   const percentage = (reduceData / data.length) * 100;
   return percentage.toFixed(2);
 };
